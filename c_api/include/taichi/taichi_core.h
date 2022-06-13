e41495840fe8d692336fb087265f2cac521eac3b
PENGUINLIONG <admin@penguinliong.moe>
2022-06-13 11:17:35 +0800

    Generate type declaration

--- c_api/include/taichi/taichi_core.h
+++ c_api/include/taichi/taichi_core.h
@@ -1,106 +1,104 @@
 #pragma once
+#include <taichi/taichi_platform.h>
 
-#include "taichi/taichi_platform.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+// alias.bool
 typedef uint32_t TiBool;
-#define TI_TRUE 1u
-#define TI_FALSE 0u
 
+// definition.false
+#define FALSE 0
+
+// definition.true
+#define TRUE 1
+
+// alias.flags
 typedef uint32_t TiFlags;
 
-typedef void *TiDispatchableHandle;
-typedef size_t TiNonDispatchableHandle;
-#define TI_NULL_HANDLE 0
+// definition.null_handle
+#define NULL_HANDLE 0
 
-typedef TiDispatchableHandle TiRuntime;
-typedef TiDispatchableHandle TiAotModule;
+// handle.runtime
+typedef struct TiRuntime_t* TiRuntime;
 
-typedef TiNonDispatchableHandle TiMemory;
-typedef TiNonDispatchableHandle TiKernel;
-typedef TiNonDispatchableHandle TiComputeGraph;
+// handle.aot_module
+typedef struct TiAotModule_t* TiAotModule;
 
+// handle.memory
+typedef struct TiMemory_t* TiMemory;
+
+// handle.kernel
+typedef struct TiKernel_t* TiKernel;
+
+// handle.compute_graph
+typedef struct TiComputeGraph_t* TiComputeGraph;
+
+// enumeration.arch
 typedef enum TiArch {
-  // TI_ARCH_ANY = 0,  // TODO: (penguinliong) Do we need this?
-  TI_ARCH_VULKAN = 1,
-} TiArch;
-TI_DLL_EXPORT TiRuntime TI_API_CALL ti_create_runtime(TiArch arch);
-TI_DLL_EXPORT void TI_API_CALL ti_destroy_runtime(TiRuntime runtime);
+  TI_ARCH_X64 = 0,
+  TI_ARCH_ARM64 = 1,
+  TI_ARCH_JS = 2,
+  TI_ARCH_CC = 3,
+  TI_ARCH_WASM = 4,
+  TI_ARCH_CUDA = 5,
+  TI_ARCH_METAL = 6,
+  TI_ARCH_OPENGL = 7,
+  TI_ARCH_DX11 = 8,
+  TI_ARCH_OPENCL = 9,
+  TI_ARCH_AMDGPU = 10,
+  TI_ARCH_VULKAN = 11,
+  TI_ARCH_MAX_ENUM = 0xffffffff,
+};
 
-typedef enum TiAllocationUsageFlagBits {
-  TI_MEMORY_USAGE_STORAGE_BIT = 1,
-  TI_MEMORY_USAGE_UNIFORM_BIT = 2,
-  TI_MEMORY_USAGE_VERTEX_BIT = 4,
-  TI_MEMORY_USAGE_INDEX_BIT = 8,
-} TiMemoryUsageFlagBits;
+// enumeration.argument_type
+typedef enum TiArgumentType {
+  TI_ARGUMENT_TYPE_I32 = 0,
+  TI_ARGUMENT_TYPE_F32 = 1,
+  TI_ARGUMENT_TYPE_NDARRAY = 2,
+  TI_ARGUMENT_TYPE_MAX_ENUM = 0xffffffff,
+};
+
+// bit_field.memory_usage
+enum TiMemoryUsageFlagBits {
+  TI_MEMORY_USAGE_STORAGE_BIT = 0,
+  TI_MEMORY_USAGE_UNIFORM_BIT = 1,
+  TI_MEMORY_USAGE_VERTEX_BIT = 2,
+  TI_MEMORY_USAGE_INDEX_BIT = 3,
+};
 typedef TiFlags TiMemoryUsageFlags;
-typedef struct {
-  uint64_t size;
-  TiBool host_write;
-  TiBool host_read;
-  TiBool export_sharing;
-  TiMemoryUsageFlags usage;
-} TiMemoryAllocateInfo;
-TI_DLL_EXPORT TiMemory TI_API_CALL
-ti_allocate_memory(TiRuntime runtime,
-                   const TiMemoryAllocateInfo *allocate_info);
-TI_DLL_EXPORT void TI_API_CALL ti_free_memory(TiRuntime runtime,
-                                              TiMemory memory);
-TI_DLL_EXPORT void *TI_API_CALL ti_map_memory(TiRuntime runtime,
-                                              TiMemory memory);
-TI_DLL_EXPORT void TI_API_CALL ti_unmap_memory(TiRuntime runtime,
-                                               TiMemory memory);
 
-typedef struct TiNdShape {
+// structure.memory_allocation_info
+struct TiMemoryAllocationInfo {
+  uint64_t size;
+  bool host_write;
+  bool host_read;
+  bool export_sharing;
+  TiMemoryUsageFlagBits usage;
+};
+
+// structure.nd_shape
+struct TiNdShape {
   uint32_t dim_count;
-  uint32_t dims[16];  // TODO: (penguinliong) give this constant a name?
-} TiNdShape;
-typedef struct TiNdArray {
+  uint32_t dims;
+};
+
+// structure.nd_array
+struct TiNdArray {
   TiMemory memory;
   TiNdShape shape;
   TiNdShape elem_shape;
-} TiNdArray;
-enum TiArgumentType {
-  TI_ARGUMENT_TYPE_I32,
-  TI_ARGUMENT_TYPE_F32,
-  TI_ARGUMENT_TYPE_NDARRAY,
 };
+
+// union.argument_value
 union TiArgumentValue {
-  int32_t i32;
-  float f32;
-  TiNdArray ndarray;
 };
+
+// structure.argument
 struct TiArgument {
   TiArgumentType type;
   TiArgumentValue value;
 };
+
+// structure.named_argument
 struct TiNamedArgument {
-  const char *name;
-  TiArgument arg;
+  const char* name;
+  TiArgument argument;
 };
-TI_DLL_EXPORT void TI_API_CALL ti_launch_kernel(TiRuntime runtime,
-                                                TiKernel kernel,
-                                                uint32_t arg_count,
-                                                const TiArgument *args);
-TI_DLL_EXPORT void TI_API_CALL
-ti_launch_compute_graph(TiRuntime runtime,
-                        TiComputeGraph compute_graph,
-                        uint32_t arg_count,
-                        const TiNamedArgument *named_args);
-TI_DLL_EXPORT void TI_API_CALL ti_submit(TiRuntime runtime);
-TI_DLL_EXPORT void TI_API_CALL ti_wait(TiRuntime runtime);
-
-TI_DLL_EXPORT TiAotModule TI_API_CALL
-ti_load_aot_module(TiRuntime runtime, const char *module_path);
-TI_DLL_EXPORT void TI_API_CALL ti_destroy_aot_module(TiAotModule mod);
-TI_DLL_EXPORT TiKernel TI_API_CALL ti_get_aot_module_kernel(TiAotModule mod,
-                                                            const char *name);
-TI_DLL_EXPORT TiComputeGraph TI_API_CALL
-ti_get_aot_module_compute_graph(TiAotModule mod, const char *name);
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
--- c_api/include/taichi/taichi_vulkan.h
+++ c_api/include/taichi/taichi_vulkan.h
@@ -1,14 +1,9 @@
 #pragma once
-
-#include "taichi/taichi_core.h"
-#define VK_NO_PROTOTYPES
+#include <taichi/taichi_core.h>
 #include <vulkan/vulkan.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef struct TiVulkanRuntimeInteropInfo {
+// structure.vulkan_runtime_interop_info
+struct TiVulkanRuntimeInteropInfo {
   uint32_t api_version;
   VkInstance instance;
   VkPhysicalDevice physical_device;
@@ -17,32 +12,11 @@ typedef struct TiVulkanRuntimeInteropInfo {
   uint32_t compute_queue_family_index;
   VkQueue graphics_queue;
   uint32_t graphics_queue_family_index;
-} TiVulkanRuntimeInteropInfo;
-TI_DLL_EXPORT TiRuntime TI_API_CALL
-ti_create_vulkan_runtime_ext(uint32_t api_version,
-                             const char **instance_extensions,
-                             uint32_t instance_extensions_count,
-                             const char **runtime_extensions,
-                             uint32_t runtime_extensions_count);
-TI_DLL_EXPORT TiRuntime TI_API_CALL
-ti_import_vulkan_runtime(const TiVulkanRuntimeInteropInfo *interop_info);
-TI_DLL_EXPORT void ti_export_vulkan_runtime(
-    TiRuntime runtime,
-    TiVulkanRuntimeInteropInfo *interop_info);
+};
 
-typedef struct TiVulkanMemoryInteropInfo {
+// structure.vulkan_memory_interop_info
+struct TiVulkanMemoryInteropInfo {
   VkBuffer buffer;
   size_t size;
   VkBufferUsageFlags usage;
-} TiVulkanMemoryInteropInfo;
-TI_DLL_EXPORT TiMemory TI_API_CALL
-ti_import_vulkan_memory(TiRuntime runtime,
-                        const TiVulkanMemoryInteropInfo *interop_info);
-TI_DLL_EXPORT void TI_API_CALL
-ti_export_vulkan_memory(TiRuntime runtime,
-                        TiMemory memory,
-                        TiVulkanMemoryInteropInfo *interop_info);
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
+};
