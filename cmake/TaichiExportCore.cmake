ae0fb2c2439dc07f38785a1930ea8aaa268ac57c
PENGUINLIONG <admin@penguinliong.moe>
2022-06-14 10:17:39 +0800

    Implement Core profile of Taichi Runtime API

--- cmake/TaichiExportCore.cmake
+++ cmake/TaichiExportCore.cmake
@@ -7,3 +7,19 @@ target_link_libraries(${TAICHI_EXPORT_CORE_NAME} PRIVATE taichi_isolated_core)
 set_target_properties(${TAICHI_EXPORT_CORE_NAME} PROPERTIES
     LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build"
     ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build")
+
+set(TAICHI_C_API_LIB_NAME taichi_c_api)
+
+file(GLOB_RECURSE C_API_SOURCE "c_api/src/*.cpp")
+add_library(${TAICHI_C_API_LIB_NAME} SHARED ${C_API_SOURCE})
+target_include_directories(${TAICHI_C_API_LIB_NAME} PUBLIC
+    ${CMAKE_CURRENT_SOURCE_DIR}
+    ${CMAKE_CURRENT_SOURCE_DIR}/c_api/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/external/spdlog/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/external/Vulkan-Headers/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/external/SPIRV-Tools/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/external/Vulkan-Headers/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/external/volk
+)
+
+target_link_libraries(${TAICHI_C_API_LIB_NAME} taichi_isolated_core)
